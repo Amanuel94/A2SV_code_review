3622ea99745ec30ccd1bec57281c0449645e9949
Amha Kindu <91547661+starlord-amha@users.noreply.github.com>
2023-01-10 02:43:22 -0800

    code-review
    Improvements:
        => Bad variable naming. Apply good coding practice
        => Unused variable 'size' in the function 'stack' It can be omitted
        => You can use a single if-else condition in your loop instead of two separate
    
    Other suggestions:
        => Only use functions when you have a REUSABLE block of code. You should put the whole body of the 'stack' function in the for-loop

--- HR-piling-up.py
+++ HR-piling-up.py
@@ -1,34 +1,32 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 
 
-def stack(st, size):
-    tot = 0
-    i, j = 0, len(st) -1
-    while tot < len(st):
-        if not tot:
-            if st[i] > st[j]:
-                top = i
-                i+=1
-                
-            else:
-                top = j
-                j-=1
-                
+def stack(blocks):
+    size=len(blocks)
+    
+    #Assume the first block has infinite length
+    #So that any block can be on top of it
+    top_block = float('inf')
+    
+    
+    left, right = 0, size -1
+    
+    while left <= right:
+        if blocks[left]>blocks[right] and blocks[left]<=top_block:
+            top_block = blocks[left] 
+            left+=1
+        elif blocks[right]>=blocks[left] and blocks[right]<=top_block:
+            top_block = blocks[right]
+            right-=1
         else:
-            if st[i] > st[j] and st[i] <= st[top]:
-                top = i
-                i+=1
-                
-            elif st[j] <= st[top]:
-                top = j
-                j-=1
-                
-            else:
-                return "No"
-        tot+=1
-    return "Yes"
+            break
+            
+    if right < left:
+        return 'Yes'
+    else:
+        return 'No'
     
 for T in range(int(input())):
     size =  int(input())
-    arr = list(map(int, input().split()))
-    print(stack(arr, size))
+    blocks = list(map(int, input().split()))
+    print(stack(blocks))
